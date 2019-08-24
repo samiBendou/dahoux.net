b99eb085b5d0ff3c198a5f42b3e44ad79d53fe43
bendou <sami.dahoux@etu.emse.fr>
2019-08-24 15:26:36 +0200

    changing picture alignment

--- src/about/About.js
+++ src/about/About.js
@@ -7,8 +7,11 @@ const About = (props) => {
     return (
         <div id="about">
             <img className="about-picture" width="100%" alt="profile" src={props.urls.picture}/>
-            <Jumbotron style={{paddingTop: 0, paddingBottom: 0, paddingRight: 0, borderRadius:0}}>
+            <Jumbotron style={{paddingTop: 0, paddingBottom: 0, paddingRight: 0, borderRadius:0, paddingLeft:0}}>
                 <div className="about-content">
+                    <div className="about-thumbnail">
+                        <img width="100%" alt="profile" src={props.urls.picture}/>
+                    </div>
                     <AboutText
                         firstName={props.firstName}
                         lastName={props.lastName}
@@ -18,9 +21,6 @@ const About = (props) => {
                         location={props.location}
                         birthday={props.birthday}
                     />
-                    <div className="about-thumbnail">
-                        <img width="100%" alt="profile" src={props.urls.picture}/>
-                    </div>
                 </div>
             </Jumbotron>
         </div>
