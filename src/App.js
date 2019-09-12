0f795dc1330f4354445390dff3559dfcb6114b20
bendou <sami.dahoux@etu.emse.fr>
2019-09-12 23:44:39 +0200

    fixing PDF generator

--- src/App.js
+++ src/App.js
@@ -1,117 +1,19 @@
-import React, {Component} from "react";
-import "./scss/App.scss";
-import About from "./about/About";
-import Contact from "./contact/Contact"
-import Skills from "./skills/Skills";
-import Timeline from "./timeline/Timeline";
-import {Container, Jumbotron} from "react-bootstrap";
-import ReactLoading from 'react-loading';
+import React from 'react';
+import Home from "./Home";
+import Pdf from "./Pdf";
+import {Route, Switch} from "react-router-dom";
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            lastName: "Last Name",
-            firstName: "First Name",
-            birthday: "1970-01-01T00:00:00Z",
-            quote: "Quote",
-            urls: {
-                facebook: "#",
-                github: "#",
-                linkedin: "#",
-                picture: "",
-                contact: null
-            },
-            items: {
-                about: [
-                    {title: "Title", text: "Text"},
-                ],
+const App = () => (
+    <div className="app">
+        <Main />
+    </div>
+);
 
-                timeline: [
-                    {
-                        title: "Title",
-                        text: "Text",
-                        date: "1970-01-01T00:00:00Z",
-                        location: {
-                            country: "FR",
-                            zip: "75001"
-                        },
-                        duration: 0,
-                        category: {
-                            tag: "Tag",
-                            color: "black"
-                        },
-                        company: {
-                            url: "#",
-                            name: "Name"
-                        }
-                    }
-                ],
-                skills: [
-                    {category: "category", label: "Label", level: 80}
-                ]
-            },
-            mail: "me@domain.dot",
-            location: {
-                country: "FR",
-                zip: "75001"
-            },
-            ready: false
-        }
-    }
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/pdf" component={Pdf}/>
+    </Switch>
+);
 
-    componentDidMount() {
-        const req = new XMLHttpRequest();
-        req.open('GET', 'https://raw.githubusercontent.com/samiBendou/dahoux.net/master/src/user.json', true);
-        req.onload = () => {
-            if (req.status === 200)
-                this.setState({...JSON.parse(req.response), ready: true});
-            else
-                console.error(req.status)
-        };
-        req.send(null);
-    }
-
-    render() {
-        let urls = this.state.urls;
-        urls.contact = urls.contact || "#contact";
-
-        if (!this.state.ready) {
-        return (
-            <div className="loader-background">
-                <div
-                    style={{
-                        position: 'absolute', left: '50%', top: '50%',
-                        transform: 'translate(-50%, -50%)',
-
-                    }}>
-                    <ReactLoading type="bubbles" color="#000000"/>
-                </div>
-            </div>);
-        } else {
-            return (
-                <div>
-                    <About
-                        lastName={this.state.lastName}
-                        firstName={this.state.firstName}
-                        quote={this.state.quote}
-                        urls={urls}
-                        items={this.state.items}
-                        birthday={this.state.birthday}
-                        location={this.state.location}
-                    />
-                    <Container>
-                        <Skills items={this.state.items.skills}/>
-                        <Timeline items={this.state.items.timeline}/>
-                        <Jumbotron id="contact">
-                            <Contact
-                                mail={this.state.mail}
-                            />
-                        </Jumbotron>
-                        <h6 className="copyright-text">© Sami Dahoux 2019, All Rights Reserved</h6>
-                    </Container>
-                </div>
-            );
-        }
-    }
-};
+export default App;
--- src/about/AboutText.js
+++ src/about/AboutText.js
@@ -6,50 +6,31 @@ import BirthdayText from "../common/BirthdayText";
 import AddressText from "../common/AddressText";
 
 export default class AboutText extends Component {
-
-    async downloadPdf() {
-        const html = (await (await fetch("http://http://dahoux.net")).text());
-
-        // eslint-disable-next-line no-undef
-        SejdaJsApi.htmlToPdf({
-            filename: `${this.props.firstName}_${this.props.lastName}_resume.pdf`,
-            /* leave blank for one long page */
-            pageSize: "a4",
-            publishableKey: "api_public_7545edc01f204628a3858d335c1c13d9",
-            htmlCode: html,
-            /* url: window.location.href */
-            always: function(){
-                // PDF download should have started
-                console.log("Download has started");
-            }
-        });
-    }
-
     render() {
         const props = this.props;
         return (
-        <div className="about-text">
-            <h1 className="about-head">
-                {`${props.firstName} ${props.lastName}`}<br/>
-                <small>{props.quote}</small>
-            </h1>
+            <div className="about-text">
+                <h1 className="about-head">
+                    {`${props.firstName} ${props.lastName}`}<br/>
+                    <small>{props.quote}</small>
+                </h1>
 
-            <BirthdayText birthday={props.birthday}/>
-            <AddressText location={props.location} county={true}/>
+                <BirthdayText birthday={props.birthday}/>
+                <AddressText location={props.location} county={true}/>
 
-            <div className="about-list">
-                <AboutList items={props.items.about}/>
+                <div className="about-list">
+                    <AboutList items={props.items.about}/>
+                </div>
+                <div className="about-skills">
+                    <SkillsList items={props.items.skills.filter(item => item.category === 1)} count={3}/>
+                </div>
+                <h3 className="about-icons">
+                    <a href={props.urls.linkedin}><FaLinkedin/></a>&nbsp;
+                    <a href={props.urls.github}><FaGithub/></a>&nbsp;
+                    <a href={props.urls.contact}><FaEnvelope/></a>&nbsp;
+                    <a href="http://localhost:3000/pdf"><FaFilePdf/></a>
+                </h3>
             </div>
-            <div className="about-skills">
-                <SkillsList items={props.items.skills.filter(item => item.category === 1)} count={3}/>
-            </div>
-            <h3 className="about-icons">
-                <a href={props.urls.linkedin}><FaLinkedin/></a>&nbsp;
-                <a href={props.urls.github}><FaGithub/></a>&nbsp;
-                <a href={props.urls.contact}><FaEnvelope/></a>&nbsp;
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a href="#" onClick={this.downloadPdf}><FaFilePdf/></a>&nbsp;
-            </h3>
-        </div>
-    );}
+        );
+    }
 };
