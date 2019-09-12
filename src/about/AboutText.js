d1ab8dc6aeac96e20543978de63226e8d1ba0387
bendou <sami.dahoux@etu.emse.fr>
2019-09-12 19:14:17 +0200

    adding PDF generator with Sejda

--- src/about/AboutText.js
+++ src/about/AboutText.js
@@ -1,12 +1,30 @@
 import React from "react";
 import AboutList from "./AboutList";
-import {FaLinkedin, FaGithub, FaEnvelope} from "react-icons/fa";
+import {FaLinkedin, FaGithub, FaEnvelope, FaFilePdf} from "react-icons/fa";
 import SkillsList from "../skills/SkillsList";
 import BirthdayText from "../common/BirthdayText";
 import AddressText from "../common/AddressText";
 
-const AboutText = (props) =>
-    (
+
+const downloadPdf = () => {
+    // eslint-disable-next-line no-undef
+    SejdaJsApi.htmlToPdf({
+        filename: "out.pdf",
+        /* leave blank for one long page */
+        pageSize: "a4",
+        publishableKey: "api_public_7545edc01f204628a3858d335c1c13d9",
+        htmlCode: document.querySelector("html").innerHTML,
+        /* url: window.location.href */
+        always: function(){
+            // PDF download should have started
+            console.log("Download has started");
+        }
+    });
+};
+
+const AboutText = (props) => {
+
+    return (
         <div className="about-text">
             <h1 className="about-head">
                 {`${props.firstName} ${props.lastName}`}<br/>
@@ -26,8 +44,11 @@ const AboutText = (props) =>
                 <a href={props.urls.linkedin}><FaLinkedin/></a>&nbsp;
                 <a href={props.urls.github}><FaGithub/></a>&nbsp;
                 <a href={props.urls.contact}><FaEnvelope/></a>&nbsp;
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <a href="#" onClick={downloadPdf}><FaFilePdf/></a>&nbsp;
             </h3>
         </div>
     );
+};
 
 export default AboutText;
