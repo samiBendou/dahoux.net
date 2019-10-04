a815db4b21fc4453befc6116845dcfd9b147113d
bendou <sami.dahoux@etu.emse.fr>
2019-10-04 15:55:04 +0200

    update data

--- src/skills/SkillsItem.js
+++ src/skills/SkillsItem.js
@@ -4,7 +4,7 @@ import '../scss/Skills.scss';
 const SkillsItem = (props) => {
     return (
         <li
-            style={{width: `${props.level}%`}}
+            style={{width: `${props.level * 10}%`}}
         >
             <h6>{props.label}</h6>
         </li>
