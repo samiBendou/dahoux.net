b720abfd7d4501c4a611d2a1ab1c208bdb54ec59
bendou <sami.dahoux@etu.emse.fr>
2019-10-10 17:08:33 +0200

    improving location fetching architecture

--- src/common/AddressText.js
+++ src/common/AddressText.js
@@ -1,49 +1,27 @@
 import React, {Component} from "react";
 import {FaMapMarker} from "react-icons/fa";
 import "./scss/Hidder.scss";
+import {fetchAddress} from "./address";
 
 export default class AddressText extends Component {
     constructor(props) {
         super(props);
         this.state = {
             city: "",
-            zip: "",
             country: "",
             county: "",
             hidden: true
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
         this.fetchLocation();
     }
 
     fetchLocation = () => {
-        const countryUrl = `https://restcountries.eu/rest/v2/alpha/${this.props.location.country}`;
-        const cityUrl = `https://geo.api.gouv.fr/communes?codePostal=${this.props.location.zip}&format=json`;
-        let cityName = "", countyCode = "";
-
-        fetch(countryUrl)
-            .then(response => response.json())
-            .then((data) => this.setState({country: data["name"]}))
-            .catch((e) => console.log("error", e));
-
-        fetch(cityUrl)
-            .then(response => response.json())
-            .then(data => {
-                countyCode = data[0]["codeDepartement"];
-                cityName = data[0]["nom"];
-                fetch(`https://geo.api.gouv.fr/departements?code=${countyCode}&fields=nom&format=json`)
-                    .then(response => response.json())
-                    .then((data) => {
-                        this.setState({city: cityName, county: data[0]["nom"]});
-                        setTimeout(() => {
-                            this.setState({hidden: false});
-                        }, 100);
-                    })
-                    .catch((e) => console.log("error", e));
-            })
-            .catch((e) => console.log("error", e));
+        fetchAddress(this.props.location).then(
+            location => this.setState({...location, hidden: false})
+        );
     };
 
     render() {
