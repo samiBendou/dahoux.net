5335c2e858498c60964245e7288af1d1cd645c3f
bendou <sami.dahoux@etu.emse.fr>
2019-10-10 21:38:53 +0200

    cleaning user fetching

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ import Loader from "./Loader";
 import Page from "./Page";
 import {fetchLocation, fetchTimelineLocation} from "./common/core/location";
 import defaultUser from "./common/core/defaultUser";
-
-const fetchUser = (url) => {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(response => response.text())
-            .then(str => resolve(JSON.parse(str)))
-            .then(json => resolve(json))
-            .catch(e => reject.log(e));
-    })
-};
+import user from "./user.json";
 
 export default class App extends Component {
     constructor(props) {
@@ -23,17 +14,13 @@ export default class App extends Component {
     }
 
     componentWillMount() {
-        fetchUser("https://raw.githubusercontent.com/samiBendou/dahoux.net/master/src/user.json")
-            .then(data => {
-                Promise.all([fetchLocation(data.location), fetchTimelineLocation(data.items.timeline)])
-                    .then((items) => {
-                        this.data = data;
-                        this.data.location = items[0];
-                        this.data.items.timeline.forEach((item, index) => item.location = items[1][index]);
-                        this.setState({ready: true});
-                    })
+        Promise.all([fetchLocation(user.location), fetchTimelineLocation(user.items.timeline)])
+            .then((items) => {
+                this.data = user;
+                this.data.location = items[0];
+                this.data.items.timeline.forEach((item, index) => item.location = items[1][index]);
+                this.setState({ready: true});
             })
-            .catch(e => console.log(e));
     }
 
     render() {
