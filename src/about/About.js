914615dcddfe6d4e3b9171c6a91adb8aa0401d39
Sami Dahoux <samidahoux@gmail.com>
2021-09-09 19:12:04 +0200

    fix: cleaning About css

--- src/about/About.js
+++ src/about/About.js
@@ -8,34 +8,30 @@ const About = (props) => {
   const data = props.data;
   return (
     <div id="about">
-      <h1 id="about-header" className="title">
+      <h1 className="title">
         <FaUserCircle className="icon-header" /> About
       </h1>
 
-      <div id="about-inner">
-        <div id="about-first">
-          <h1 id="about-name" className="about-left-item">
-            {`${data.firstName} ${data.lastName}`}
-          </h1>
+      <div className="inner">
+        <div className="first">
+          <h1 className="name left-item">{`${data.firstName} ${data.lastName}`}</h1>
 
-          <h2 id="about-title" className="about-left-item">
-            {data.quote}
-          </h2>
+          <h2 className="quote left-item">{data.quote}</h2>
 
-          <h2 id="about-links" className="about-left-item">
+          <h2 className="left-item">
             <LinksBar data={data} />
           </h2>
 
-          <h3 id="about-location" className="about-left-item">
+          <h3 className="location left-item">
             <LocationText location={data.location} county={true} />
           </h3>
         </div>
 
-        <div id="about-second">
+        <div className="second">
           {data.items.about.map((item) => (
             <span key={item.text}>
-              <h2 className="about-right-item">{item.title}</h2>
-              <p className="about-right-item">{item.text}</p>
+              <h2 className="right-item">{item.title}</h2>
+              <p className="right-item">{item.text}</p>
             </span>
           ))}
         </div>
