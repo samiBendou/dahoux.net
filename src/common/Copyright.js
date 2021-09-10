5214c9421416cce896e268cf772c201572f1059b
Sami Dahoux <samidahoux@gmail.com>
2021-09-10 12:30:36 +0200

    fix: typo on copyright

--- src/common/Copyright.js
+++ src/common/Copyright.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Copyright = () => <h2 id="copyright">© Sami Dahoux 2017 - 2021, All Rights Reserved</h2>;
+const Copyright = () => <h2 id="copyright">© Sami Dahoux 2017-2021, All Rights Reserved</h2>;
 
 export default Copyright;
